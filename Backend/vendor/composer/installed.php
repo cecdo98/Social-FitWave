a7bbcc3baf82e89721d744625292fcb4a53d18d9
cecdo98 <carlosemanuel98@hotmail.com>
2025-02-26 12:14:53 +0000

    composer

---
+++ Backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'carlo/backend',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ac8dff9dde6f443823d9ead8f65d208dcb527029',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'carlo/backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ac8dff9dde6f443823d9ead8f65d208dcb527029',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
