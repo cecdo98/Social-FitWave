8a6c52fa8a37bd33aaca8cc683f0aa5cd807d5b9
cecdo98 <carlosemanuel98@hotmail.com>
2025-03-03 20:37:16 +0000

    profile.v.2

--- Frontend/social-fitwave/src/assets/Blocks/ProfileBlock.jsx
+++ Frontend/social-fitwave/src/assets/Blocks/ProfileBlock.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import useUserProfile from "./useUserProfile";
+import '../../Css/BlockCss/ModalBlock.Css'
 
 function ProfileBlock({ email, token }) {
     const {
@@ -13,55 +14,60 @@ function ProfileBlock({ email, token }) {
         ToggleModal
     } = useUserProfile(email, token);
 
-
-    
-
     return (
         <>
             <button onClick={handleEvent}>Perfil</button>
 
             {profileModalOpen && user && (
-                <div>
-                    <h2>Perfil do Utilizador</h2>
-                    <form onSubmit={updateProfile}>
-                        <div>
-                            <p><strong>Nome antigo:</strong> {user.name}</p>
-                            <label>Nome:</label>
-                            <input
-                                type="text"
-                                value={editProfile.name}
-                                onChange={(e) => setEditProfile({ ...editProfile, name: e.target.value })}
-                            />
-                        </div>
-                        <div>
-                            <p><strong>Email antigo:</strong> {user.email}</p>
-                            <label>Email:</label>
-                            <input
-                                type="email"
-                                value={editProfile.email}
-                                onChange={(e) => setEditProfile({ ...editProfile, email: e.target.value })}
-                            />
-                        </div>
-                        <div>
-                            <label>Nova Senha:</label>
-                            <input
-                                type="password"
-                                value={editProfile.password}
-                                onChange={(e) => setEditProfile({ ...editProfile, password: e.target.value })}
-                            />
-                        </div>
-                        <div>
-                            {/*falta a foto de perfil atual para atualizar */}
-                            <label>Foto de Perfil:</label>
-
-                        </div>
-                        <div>
-                            <button type="submit">Atualizar</button>
-                        </div>
-                        <div>
-                            <button onClick={ToggleModal}>Fechar</button>
-                        </div>
-                    </form>
+                <div className="modal">
+                    <div className="modalOverlay">
+                        <form className="modal-content" onSubmit={updateProfile}>
+                            <h2 className="Titulo">Perfil do Utilizador</h2>
+                            <div className="modalBody"> 
+                                <div>
+                                    {/*falta a foto de perfil atual para atualizar */}
+                                    <label>Foto de Perfil:</label>
+                                    <img src="http://localhost/Social-FitWave/Backend/uploads/default_profile_picture.jpg" width="100" height="100"/>
+                                </div>
+                                <div>
+                                    <div>
+                                        <p><strong>Nome antigo:</strong> {user.name}</p>
+                                        <label>Nome:</label>
+                                        <input
+                                            type="text"
+                                            value={editProfile.name}
+                                            onChange={(e) => setEditProfile({ ...editProfile, name: e.target.value })}
+                                        />
+                                    </div>
+                                    <div>
+                                        <p><strong>Email antigo:</strong> {user.email}</p>
+                                        <label>Email:</label>
+                                        <input
+                                            type="email"
+                                            value={editProfile.email}
+                                            onChange={(e) => setEditProfile({ ...editProfile, email: e.target.value })}
+                                        />
+                                    </div>
+                                    <div >
+                                        <label>Nova Senha:</label>
+                                        <input
+                                            type="password"
+                                            value={editProfile.password}
+                                            onChange={(e) => setEditProfile({ ...editProfile, password: e.target.value })}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="modalFooter">
+                                <div>
+                                    <button type="submit">Atualizar</button>
+                                </div>
+                                <div>
+                                    <button onClick={ToggleModal}>Fechar</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             )}
         </>
